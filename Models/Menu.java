508549dbac23b605d5fb8e905d554fbc0ef1e6b4
Mahmoud <mahmoudlabidi@yahoo.fr>
2022-11-19 15:28:09 -0500

    added login menu option

--- Models/Menu.java
+++ Models/Menu.java
@@ -3,6 +3,7 @@ import com.sun.source.tree.WhileLoopTree;
 import java.util.*;
 public class Menu {
     static LoginController logCon = new LoginController();
+    static ResidentController resCon;
 
     private User loggedUser = null;
     private int indexPage = 0;
@@ -16,13 +17,13 @@ public class Menu {
         Scanner reader = new Scanner(System.in);
         int choix  = -1 ;
         while ( choix != 0) {
-            System.out.print("Choisir une option : ");
+            System.out.println("Choisir une option : ");
             System.out.println("[1] Se connecter ");
             System.out.println("[2] S'inscrire");
             System.out.println("[0] Quitter ");
             choix = reader.nextInt();
             if (choix == 1) {
-                loggedUser = logCon.login() ;
+                loggedUser = logCon.login();
                 if (loggedUser != null) {
                     displayMain();
                 }
@@ -40,17 +41,90 @@ public class Menu {
         }
     }
     public void displayMain() {
-        System.out.println("omek kahba");
-
+        resCon = new ResidentController(loggedUser.getId());
+        Scanner reader = new Scanner(System.in);
+        int choix = -1;
+        while (choix != 0) {
+            System.out.println("Choisir une option : ");
+            System.out.println("[1] Mes bacs");
+            System.out.println("[2] Municipalité");
+            System.out.println("[3] Metriques");
+            System.out.println("[4] Consommateur");
+            System.out.println("[0] Quitter");
+            choix = reader.nextInt();
+            switch (choix) {
+                case 0:
+                    System.out.println("Vous avez choisi de quitter l'application ");
+                    break;
+                case 1:
+                    displayBacPage();
+                    break;
+                case 2:
+                    displayMunicipPage();
+                    break;
+                case 3:
+                    displayMetriquesPage();
+                    break;
+                case 4:
+                    displayConsomPage();
+                    break;
+            }
+        }
     }
 
     public void displayBacPage() {
-        // TODO - implement Menu.displayBacPage
+        Scanner reader = new Scanner(System.in);
+        int choix  = -1 ;
+        while (choix != 0) {
+            System.out.println("Choisir une option : ");
+            System.out.println("[1] Ajouter mes bacs");
+            System.out.println("[2] Supprimer un bac");
+            System.out.println("[3] Voir mes bacs");
+            System.out.println("[0] quitter");
+            choix = reader.nextInt();
+            switch (choix) {
+                case 0:
+                    System.out.println("Vous avez choisi de quitter l'application ");
+                    break;
+                case 1:
+                    resCon.addBac();
+                    break;
+                case 2:
+                    resCon.deleteBac();//lezem thot lbac ili thebou yetnaha
+                    break;
+                case 3:
+                    resCon.viewBacs();
+                    break;
+            }
+        }
         throw new UnsupportedOperationException();
     }
 
     public void displayMetriquesPage() {
-        // TODO - implement Menu.displayMetriquesPage
+        Scanner reader = new Scanner(System.in);
+        int choix  = -1 ;
+        while (choix != 0) {
+            System.out.println("Choisir une option : ");
+            System.out.println("[1] Voir les metriques");
+            System.out.println("[2] Supprimer un bac");
+            System.out.println("[3] Voir mes bacs");
+            System.out.println("[0] quitter");
+            choix = reader.nextInt();
+            switch (choix) {
+                case 0:
+                    System.out.println("Vous avez choisi de quitter l'application ");
+                    break;
+                case 1:
+                    resCon.addBac();
+                    break;
+                case 2:
+                    resCon.deleteBac();//lezem thot lbac ili thebou yetnaha
+                    break;
+                case 3:
+                    resCon.viewBacs();
+                    break;
+            }
+        }
         throw new UnsupportedOperationException();
     }
 
