8c03ed5a96a7a32fd7bb2109c972b91de433dc20
Mahmoud <mahmoudlabidi@yahoo.fr>
2022-11-19 14:14:04 -0500

    added login menu option

--- Models/Menu.java
+++ Models/Menu.java
@@ -1,6 +1,10 @@
-public class Menu {
+import com.sun.source.tree.WhileLoopTree;
 
-    private User loggedUser;
+import java.util.*;
+public class Menu {
+    static LoginController logCon = new LoginController();
+
+    private User loggedUser = null;
     private int indexPage = 0;
 
     public void open() {
@@ -9,10 +13,34 @@ public class Menu {
     }
 
     public void displayLoginPage() {
-        System.out.println("Hello");
+        Scanner reader = new Scanner(System.in);
+        int choix  = -1 ;
+        while ( choix != 0) {
+            System.out.print("Choisir une option : ");
+            System.out.println("[1] Se connecter ");
+            System.out.println("[2] S'inscrire");
+            System.out.println("[0] Quitter ");
+            choix = reader.nextInt();
+            if (choix == 1) {
+                loggedUser = logCon.login() ;
+                if (loggedUser != null) {
+                    displayMain();
+                }
+            } else if (choix == 2) {
+                loggedUser = logCon.createUser();
+                System.out.println("Votre compte a été crée.");
+                System.out.println("Vous pouvez maintenent choisir l'option se connecter ");
+            }
+            else if(choix == 0) {
+                System.out.println("Vous avez choisi de quitter l'application ");
+            }
+            else {
+                System.out.println("Veillez choisir uniquement un des 3 choix proposés");
+            }
+        }
     }
-
     public void displayMain() {
+        System.out.println("omek kahba");
 
     }
 
